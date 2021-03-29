c9e3c189e11b686c95558c384525efb057457d4e
Ivan Sena <ivanprojetosti@gmail.com>
2021-03-29 20:28:21 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  form: FormGroup;
 
-  constructor() {}
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+this.createForm();
+
+  }
+
+createForm(){
+  this.form = this.formBuilder.group({
+    aluno: ['', Validators.required],
+    email:['', Validators.required],
+    telefone:['', Validators.required],
+    dtnascimento:['', Validators.required],
+    nomepai:['', Validators.required],
+    nomemae:['', Validators.required]
+  })
+}
+salvar(){
+  console.log(this.form.value);
+  
+}
 
 }
